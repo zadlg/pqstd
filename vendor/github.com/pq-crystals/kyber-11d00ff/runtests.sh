8ef4831737bb52c89d1799e23fce1954e8aa3c8c
zadig <119496894+zadlg@users.noreply.github.com>
2024-03-28 18:08:45 +0100

    Add support for Bazel 7. (#1)
    Add support for Bazel 7.

---
+++ vendor/github.com/pq-crystals/kyber-11d00ff/runtests.sh
@@ -0,0 +1,34 @@
+#!/bin/sh -e
+nproc="${nproc:-2}"
+
+ARCH="${ARCH:-amd64}"
+ARCH="${TRAVIS_CPU_ARCH:-$ARCH}"
+
+if [ "$ARCH" = "amd64" -a "$TRAVIS_OS_NAME" != "osx" ]; then
+  DIRS="ref avx2"
+else
+  DIRS="ref"
+fi
+
+if [ "$ARCH" = "amd64" -o "$ARCH" = "arm64" ]; then
+  export CC=/usr/bin/gcc
+#  export CFLAGS="-fsanitize=undefined,address ${CFLAGS}"
+fi
+
+for dir in $DIRS; do
+  make -j$(nproc) -C $dir clean
+  make -j$(nproc) -C $dir
+  for alg in 512 768 1024; do
+    valgrind --vex-guest-max-insns=25 ./$dir/test/test_kyber$alg
+    echo test_kyber$alg
+    ./$dir/test/test_kyber$alg 
+#    PID1=$!
+#    echo testvec$alg
+    ./$dir/test/test_vectors$alg > tvecs$alg 
+#    PID2=$!
+#    wait $PID1 $PID2
+  done
+  shasum -a256 -c SHA256SUMS
+done
+
+exit 0
